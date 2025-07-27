b1d892328e8ac7edc4bbbf459faccc57fd955b48
Truongmyduyen040805@gmail.com <Truongmyduyen040805@gmail.com>
2025-07-28 04:42:53 +0700

    Leu leu

---
+++ CODEBTVNBUOI1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main\
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập tên của bạn: ");
+        String ten = scanner.nextLine();
+
+        System.out.println("Chào bạn " + ten + "!");
+    }
+}
---
+++ code1/code2/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so nguyen a:");
+        int a= sc.nextInt();
+        sc.nextLine();
+        System.out.print("Nhap so nguyen b:");
+        int b= sc.nextInt();
+        sc.nextLine();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+
+            int
+        }
+    }
